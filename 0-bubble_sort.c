650768e06af7bb3ff8849c45497bbc459baf94a4
Proser-V <10530@hotbertonstudents.com>
2025-04-08 16:17:26 +0200

    0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -15,12 +15,15 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t index1, index2, temp;
+	int swapped;
 
 	if (array == NULL || size == 0)
 		return;
 
 	for (index1 = 0; index1 < size - 1; index1++)
 	{
+		swapped = 0;
+
 		for (index2 = 0; index2 < (size - index1 - 1); index2++)
 		{
 			if (array[index2] > array[index2 + 1])
@@ -28,8 +31,11 @@ void bubble_sort(int *array, size_t size)
 				temp = array[index2];
 				array[index2] = array[index2 + 1];
 				array[index2 + 1] = temp;
+				swapped = 1;
 				print_array(array, size);
 			}
 		}
+		if (swapped == 0)
+			break;
 	}
 }
