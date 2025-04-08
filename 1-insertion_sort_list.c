9c018682cccbd10a3829216ff3b3c77b19b20258
Proser-V <10530@hotbertonstudents.com>
2025-04-08 16:45:13 +0200

    1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -16,27 +16,28 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *current, *next, *temp;
 
-	if (list == NULL || list->next == NULL)
+	if (*list == NULL || (*list)->next == NULL)
 		return;
-	current = *list;
 
-	while (current->next != NULL)
+	current = (*list)->next;
+
+	while (current != NULL)
 	{
 		next = current->next;
-		while (next->prev != NULL && next->n < next->prev->n)
+		while (current->prev != NULL && current->n < current->prev->n)
 		{
-			if (next->n > current->n)
-			{
-				current->next = next->next;
-				next->prev = current->prev;
-				current->prev = next;
-				next->next = current;
-				next->next->prev = current;
-				temp = next;
-				next = current;
-				current = temp;
-			}
+			current->prev->next = current->next;
+			if (current->next != NULL)
+				current->next->prev = current->prev;
+
+			current->next = current->prev;
+			current->prev = current->next->prev;
+			current->next->prev = current;
+			if (current->prev == NULL)
+			*list = current;
+
 			print_list(*list);
 		}
+			current = next;
 	}
 }
